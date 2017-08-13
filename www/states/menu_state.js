70e3f1d24c215475d2fb0a9aac40279006759951
Marek Dziewit <marek.dziewit@netguru.co>
2017-08-13 13:15:23 +0200

    Remove scores for now

--- www/states/menu_state.js
+++ www/states/menu_state.js
@@ -12,15 +12,15 @@ menu_state.prototype = {
         start_btn.inputEnabled = true;
         start_btn.events.onInputDown.add(this.start_game, this);
 
-        scores_btn = TanksUtil.insertCentered(
-            this.game, this.game.world.centerX, (this.game.height * 0.80), 'scores_button');
-        scores_btn.inputEnabled = true;
-        scores_btn.events.onInputDown.add(this.show_scores, this);
+        // scores_btn = TanksUtil.insertCentered(
+        //     this.game, this.game.world.centerX, (this.game.height * 0.80), 'scores_button');
+        // scores_btn.inputEnabled = true;
+        // scores_btn.events.onInputDown.add(this.show_scores, this);
     },
     preload: function () {
         this.game.load.image('logo', 'images/tanks_logo.jpg');
         this.game.load.image('start_button', 'images/start_game_button.png');
-        this.game.load.image('scores_button', 'images/best_scores_button.png');
+        // this.game.load.image('scores_button', 'images/best_scores_button.png');
     },
     start_game: function () {
         this.game.state.start('PlayState')
